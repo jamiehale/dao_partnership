b120e96514cc25eed8be209e237721f6c98c9f7f
Jamie Hale <jamie@smallarmyofnerds.com>
2016-06-15 00:04:37 -0400

    Add paid shares

--- contract.sol
+++ contract.sol
@@ -5,15 +5,24 @@ contract Partnership
 	event Deposit(address _from, uint _value);
 	event ConfirmationRequired(bytes32 _operation, address _initiator, address _to, uint _value, bytes _data);
 	event TransactionSent(bytes32 _transaction, address _finalSigner, address _to, uint _value, bytes _data);
-	
+
+	/// Price in wei of each equal share of the partnership
+	uint public sharePrice;
+
+	/// Flag indicating whether or not all partners have paid for their share
+	bool public funded;
+
 	/// Array of partner addresses
-	address [] partners;
-	
-	/// Collection of partner addresses (1 => 'partner', 0/unset => 'not partner')
-	mapping(address => uint) public partnerRecords;
+	address [] public partners;
+
+	/// Collection of partner records
+	mapping(address => Partner) public partnerRecords;
 	
 	/// Count of partners
 	uint public partnerCount;
+
+	/// Count of partners who have paid for their share
+	uint public paidPartnerCount;
 	
 	/// Collection of pending transactions (ie send X ETH to Y with Z data)
 	mapping(bytes32 => Transaction) public transactions;
@@ -21,6 +30,14 @@ contract Partnership
 	/// Collection of pending operations (ie internal function calls)
 	mapping(bytes32 => Operation) public operations;
 	
+	struct Partner {
+		bool isPartner;
+		/// Flag indicating that the partner has paid for their share
+		bool paid;
+		/// Total amount loaned to the partnership by the partner
+		uint loanBalance;
+	}
+
 	struct Transaction {
 		address to;
 		uint value;
@@ -36,6 +53,11 @@ contract Partnership
 		mapping(address => uint) votes;
 	}
 
+	modifier onlyFunded {
+		if (funded)
+		_
+	}
+
 	modifier onlyPartner {
 		if (isPartner(msg.sender))
 		_
@@ -48,25 +70,55 @@ contract Partnership
 		_
 	}
 	
-	function Partnership(address[] _partners) {
+	function Partnership(address[] _partners, uint _sharePrice) {
+		funded = false;
+		sharePrice = _sharePrice;
 		partners = _partners;
 		for (uint i = 0; i < _partners.length; i++) {
-			partnerRecords[_partners[i]] = 1;
+			partnerRecords[_partners[i]].isPartner = true;
 		}
 		partnerCount = _partners.length;
 	}
 	
 	function() {
-		if (msg.value > 0)
+		if (msg.value > 0) {
+			if (funded) {
+				if (partnerRecords[msg.sender].isPartner) {
+					partnerRecords[msg.sender].loanBalance += msg.value;
+				}
+			}
+			else {
+				if (partnerRecords[msg.sender].isPartner) {
+					if (partnerRecords[msg.sender].paid) {
+						throw;
+					}
+					else {
+						if (msg.value == sharePrice) {
+							partnerRecords[msg.sender].paid = true;
+							paidPartnerCount += 1;
+							if (paidPartnerCount == partnerCount) {
+								funded = true;
+							}
+						}
+						else {
+							throw;
+						}
+					}
+				}
+				else {
+					throw;
+				}
+			}
 			Deposit(msg.sender, msg.value);
+		}
 	}
 	
 	function isPartner(address _address) returns (bool) {
-		return partnerRecords[_address] == 1;
+		return partnerRecords[_address].isPartner;
 	}
 	
 	/// Adds a proposed transaction to be confirmed by other partners
-	function proposeTransaction(address _to, uint _value, bytes _data) onlyPartner external returns (bytes32) {
+	function proposeTransaction(address _to, uint _value, bytes _data) onlyFunded onlyPartner external returns (bytes32) {
 		bytes32 hash = sha3(msg.data, block.number);
 		
 		var transaction = transactions[hash];
@@ -83,7 +135,7 @@ contract Partnership
 	}
 	
 	/// Confirms an existing proposed transaction and executes it if all partners have confirmed
-	function confirmTransaction(bytes32 _id) onlyPartner external {
+	function confirmTransaction(bytes32 _id) onlyFunded onlyPartner external {
 		var transaction = transactions[_id];
 		
 		if (transaction.to == 0)
@@ -102,11 +154,7 @@ contract Partnership
 		}
 	}
 	
-	function kill(address _to) onlyAllPartners(sha3(msg.data)) external {
-		suicide(_to);
-	}
-	
-	function dissolve() onlyAllPartners(sha3(msg.data)) external {
+	function dissolve() onlyFunded onlyAllPartners(sha3(msg.data)) external {
 		uint payout = this.balance / partnerCount;
 		for ( uint i = 0; i < partnerCount; i++ ) {
 			partners[i].send(payout);
