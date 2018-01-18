743e138fcc615abe4249bcc21e90d5cd73ceefd6
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-01-18 19:46:20 +0100

    add attack; stub other tests

--- test/Partnership.js
+++ test/Partnership.js
@@ -24,6 +24,7 @@ contract('Partnership', function(accounts) {
     partnership = await Partnership.new([partner1, partner2, partner3], amount);
     await web3.eth.sendTransaction({from:partner1, to:partnership.address, value: amount});
     await web3.eth.sendTransaction({from:partner2, to:partnership.address, value: amount});
+    
     // This should fail because it is not funded yet. Only partners should
     // be able to send until funding is complete.
     try {
@@ -34,14 +35,14 @@ contract('Partnership', function(accounts) {
       assert(revert);
     }
     // why does the above work but this does not?
-//    await expectThrow(web3.eth.sendTransaction({from:attacker1, to:partnership.address, value: amount}));
+    // expectThrow(web3.eth.sendTransaction({from:attacker1, to:partnership.address, value: amount}));
 
     await web3.eth.sendTransaction({from:partner3, to:partnership.address, value: amount});
     // since funding is now complete, the customer should be able to send funds
     await web3.eth.sendTransaction({from:customer1, to:partnership.address, value: amount});
   });
 
-  it('only partners should be able to propose transactions', async function(){
+  it('should allow only partners to propose transactions', async function(){
     partnership = await Partnership.new([partner1, partner2], amount);
     await web3.eth.sendTransaction({from:partner1, to:partnership.address, value: amount});
     await web3.eth.sendTransaction({from:partner2, to:partnership.address, value: amount});
@@ -54,10 +55,26 @@ contract('Partnership', function(accounts) {
     var txn2 = await partnership.proposeTransaction(customer2, amount, 0, "refund", {from:partner1});
     var txnId2 = txn2.logs[0].args._id;
     await expectThrow(partnership.cancelTransaction(txnId2,{from:partner2}));
+    await expectThrow(partnership.cancelTransaction(txnId2,{from:attacker1}));
     await partnership.cancelTransaction(txnId2,{from:partner1});
 
     // an unknown party should not be able to propose a transaction
     await expectThrow(partnership.proposeTransaction(attacker1, amount, 0, "fraud",{from:attacker1}));
+  });
+
+  it('should allow only partners to make loans and withdraw them', async function(){
+    // create fund
+    // send loan
+    // create proposal to pay back loan
+    // approve payback proposal
+    // partner who made the loan makes a withdrawal
+  });
+
+  it('should dissolve a fund', async function(){
+    // create fund
+    // create proposal to dissolve
+    // approve dissolution proposal
+    // recipient should have the eth
   });
 
 });
