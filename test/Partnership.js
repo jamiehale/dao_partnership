a7f4e0b6e07802ea329efcb259b6dd10af6d1681
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-01-27 18:51:05 +0100

    add test of loan functionality

--- test/Partnership.js
+++ test/Partnership.js
@@ -15,6 +15,7 @@ contract('Partnership', function(accounts) {
   let customer2 = accounts[7];
   let partnership;
   let amount = new web3.BigNumber(web3.toWei(0.005, "ether"));
+  let loan = new web3.BigNumber(web3.toWei(0.003, "ether"));
 
   before(async function(){
 	});
@@ -56,26 +57,45 @@ contract('Partnership', function(accounts) {
     var txnId2 = txn2.logs[0].args._id;
     await expectThrow(partnership.cancelTransaction(txnId2,{from:partner2}));
     await expectThrow(partnership.cancelTransaction(txnId2,{from:attacker1}));
-    await partnership.cancelTransaction(txnId2,{from:partner1});
+    var cancellation = await partnership.cancelTransaction(txnId2,{from:partner1});
+    assert(cancellation.logs[0].event === 'TransactionCanceled');
 
     // an unknown party should not be able to propose a transaction
     await expectThrow(partnership.proposeTransaction(attacker1, amount, 0, "fraud",{from:attacker1}));
   });
 
   it('should allow only partners to make loans and withdraw them', async function(){
-    // create fund
-    // send loan
-    // create proposal to pay back loan
-    // approve payback proposal
-    // partner who made the loan makes a withdrawal
+    // create fund with two partners
+    partnership = await Partnership.new([partner1, partner2], amount);
+    await web3.eth.sendTransaction({from:partner1, to:partnership.address, value: amount});
+    await web3.eth.sendTransaction({from:partner2, to:partnership.address, value: amount});
+    // parter1 sends loan
+    await web3.eth.sendTransaction({from:partner1, to:partnership.address, value: loan});
+    // partner2 creates proposal to pay back loan
+    var callData = partnership.contract.repayLoan.getData(partner1, loan);
+    var txn1 = await partnership.proposeTransaction(partnership.address, 0, callData, "repay loan", {from:partner2});
+    assert(txn1.logs[0].event === 'TransactionProposed');
+    var txnId1 = txn1.logs[0].args._id;
+    // partner1 approves payback proposal
+    var confirmation = await partnership.confirmTransaction(txnId1,{from:partner1});
+    assert(confirmation.logs[0].event === 'TransactionPassed');
+    // partner1 can't withdraw yet.
+    await expectThrow(partnership.withdraw(loan,{from:partner1}));
+    // partner2 executes transaction
+    var execution = await partnership.executeTransaction(txnId1,{from:partner2});
+    assert(execution.logs[0].event === 'TransactionSent');
+    // partner1 makes a withdrawal of the loan
+    var withdrawal = await partnership.withdraw(loan, {from:partner1});
+    assert(withdrawal.logs[0].event === 'Withdrawal');
   });
 
-  it('should dissolve a fund', async function(){
+  // dissolving a fund is not a good idea because it abandons tokens.
+//  it('should allow partners to dissolve a fund', async function(){
     // create fund
     // create proposal to dissolve
     // approve dissolution proposal
     // recipient should have the eth
-  });
+//  });
 
 });
 /*
